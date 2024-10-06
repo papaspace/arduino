f410b0584c319ac236f6f9e88dd1536ab73afea5
Michael Braunstingl <m.braunstingl@gmail.com>
2024-10-06 21:25:32 +0200

    TestSerial: Connection class and stability of connection test

--- TestSerial/TestSerial.ino
+++ TestSerial/TestSerial.ino
@@ -2,11 +2,16 @@ int x;
 
 void setup() { 
   Serial.begin(115200); 
-  Serial.setTimeout(1); 
+  Serial.setTimeout(1);
+  delay(100);
+  Serial.write("Hello, I am ready");
 }
 
-void loop() { 
-  while (!Serial.available()); 
+void loop() {
+
+  while (!Serial.available());          // Do nothing as long as the serial buffer is empty
+  delay(10);                            // We still need a small delay until we can read data from the serial buffer
   x = Serial.readString().toInt(); 
-  Serial.print(x + 1); 
-} 
+  Serial.print(x + 1);
+
+}
--- TestSerial/TestSerial.py
+++ TestSerial/TestSerial.py
@@ -1,15 +1,57 @@
 import serial
+import serial.tools.list_ports
 import time
 
-arduino = serial.Serial(port='COM2', baudrate=115200, timeout=.1)
 
-def write_read(x):
-	   arduino.write(bytes(x, 'utf-8'))
-	   time.sleep(0.05)
-	   data = arduino.readline()
-	   return data
+class arduino:
+  def __init__(self, port_name):
+    self.port_name=port_name
+    self.connection=None
+    
+  def connect(self, baudrate=115200, timeout=0.1):
+    print('Connecting (port="'"%s>"'", baud rate=%d) ...'%(self.port_name, baudrate))
+    ports=serial.tools.list_ports.comports()
+    for port in ports:
+      if port.device==self.port_name:
+        try:
+          self.connection=serial.Serial(port=port.device, baudrate=baudrate, timeout=timeout)
+          print('Connection established')
+          while True:
+            msg=self.connection.readline()
+            if msg==b'':  
+              time.sleep(0.1)
+            else:
+              print('Handshake message: %s'%msg)
+              break
+          return self.connection
+        except Exception as ex:
+          print('Unable to establish connection: %s'%ex)
+          return self.connection
 
-while True:
-	   num = input("Enter a number: ")
-	   value = write_read(num)
-	   print(value)
+    raise ValueError('Unable to find port '+self.port_name)
+
+  def write_read(self, x):
+    self.connection.write(bytes(x, 'utf-8'))
+    time.sleep(0.2)
+    data=self.connection.readline()
+    return data
+
+arduino=arduino('/dev/ttyACM0')
+arduino.connect()
+
+if arduino.connection:
+
+  while False:
+    num=input("Enter a number: ")
+    value=arduino.write_read(num)
+    print(value)
+
+  x=110
+  while True:
+    msg=arduino.write_read('%d'%x)
+    y=int(msg)
+    print('tx=%d msg=%d'%(x, y))
+    if y-x!=1:
+      print('!!!')
+    x=x+1
+
